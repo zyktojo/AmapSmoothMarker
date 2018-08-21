6aaa22b0d3d31aa704ccfd2a5c1fb459e06227b6
mo <2839226980@qq.com>
2018-08-21 09:19:31 +0800

    恢复乱码。2018年8月21日09:18:41

--- app/src/main/java/com/marker/mo/amapsmoothmarker/MainActivity.java
+++ app/src/main/java/com/marker/mo/amapsmoothmarker/MainActivity.java
@@ -26,30 +26,30 @@ import java.util.Random;
 
 /**
  * author: Ivan Zhang
- * �ߵµ�ͼ��ͼ��ƽ���ƶ�����Demo
+ * 高德地图，图标平滑移动功能Demo
  */
 public class MainActivity extends AppCompatActivity {
 
     MapView mMapView = null;
     AMap aMap;
 
-    private double[] coords;//·�����������,������ո���
-    private List<LatLng> carsLatLng;//��̬����������
+    private double[] coords;//路线坐标点数据,不断清空复用
+    private List<LatLng> carsLatLng;//静态车辆的数据
     private List<LatLng> goLatLng;
-    private List<Marker> showMarks;//��̬����ͼ��
-    private List<SmoothMoveMarker> smoothMarkers;//ƽ���ƶ�ͼ�꼯��
-    //����
+    private List<Marker> showMarks;//静态车辆图标
+    private List<SmoothMoveMarker> smoothMarkers;//平滑移动图标集合
+    //经度
     private double lng = 0.0;
-    //γ��
+    //纬度
     private double lat = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //��ȡ��ͼ�ؼ�����
+        //获取地图控件引用
         mMapView = (MapView) findViewById(R.id.map);
-        //��activityִ��onCreateʱִ��mMapView.onCreate(savedInstanceState)��������ͼ
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
         mMapView.onCreate(savedInstanceState);
         if (aMap == null) {
             aMap = mMapView.getMap();
@@ -62,24 +62,24 @@ public class MainActivity extends AppCompatActivity {
         Button put = (Button) findViewById(R.id.put);
         Button run = (Button) findViewById(R.id.run);
 
-        //���뾲̬����
+        //放入静态车辆
         put.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //��յ�ͼ������
-                if (smoothMarkers != null) {//��ն�̬marker
+                //清空地图覆盖物
+                if (smoothMarkers != null) {//清空动态marker
                     for (int i = 0; i < smoothMarkers.size(); i++) {
                         smoothMarkers.get(i).destroy();
                     }
                 }
-                //����ɼ���
+                //清除旧集合
                 if (showMarks == null) {
                     showMarks = new ArrayList<Marker>();
                 }
                 for (int j = 0; j < showMarks.size(); j++) {
                     showMarks.get(j).remove();
                 }
-                //���η��뾲̬ͼ��
+                //依次放入静态图标
                 for (int i = 0; i < carsLatLng.size(); i++) {
                     BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.drawable.car_up);
                     lng = Double.valueOf(carsLatLng.get(i).longitude);
@@ -90,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
                     showMarks.add(aMap.addMarker(markerOptions));
                     Animation startAnimation = new AlphaAnimation(0, 1);
                     startAnimation.setDuration(600);
-                    //�������о�ֹ���ĽǶ�
+                    //设置所有静止车的角度
 //                            showMarks.get(i).setRotateAngle(Float.valueOf(listBaseBean.datas.get(i).angle));
                     showMarks.get(i).setAnimation(startAnimation);
                     showMarks.get(i).setRotateAngle(new Random().nextInt(359));
@@ -100,34 +100,34 @@ public class MainActivity extends AppCompatActivity {
         });
 
         /**
-         * չʾƽ���ƶ�����
+         * 展示平滑移动车辆
          */
         run.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (smoothMarkers != null) {//��ն�̬marker
+                if (smoothMarkers != null) {//清空动态marker
                     for (int i = 0; i < smoothMarkers.size(); i++) {
                         smoothMarkers.get(i).destroy();
                     }
                 }
-                //����ɼ���
+                //清除旧集合
                 if (showMarks == null) {
                     showMarks = new ArrayList<Marker>();
                 }
-                //�����̬marker
+                //清除静态marker
                 for (int j = 0; j < showMarks.size(); j++) {
                     showMarks.get(j).remove();
                 }
-                smoothMarkers = null;//��վ�����
+                smoothMarkers = null;//清空旧数据
                 smoothMarkers = new ArrayList<SmoothMoveMarker>();
                 BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.drawable.car_up);
-                //ѭ��
+                //循环
                 for (int i = 0; i < carsLatLng.size(); i++) {
-                    //����·��
+                    //放入路线
                     double[] newoords = {Double.valueOf(carsLatLng.get(i).longitude), Double.valueOf(carsLatLng.get(i).latitude),
                             Double.valueOf(goLatLng.get(i).longitude), Double.valueOf(goLatLng.get(i).latitude)};
                     coords = newoords;
-                    //�ƶ�����
+                    //移动车辆
                     movePoint(icon);
                 }
             }
@@ -135,21 +135,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initData() {
-        //������
+        //出发地
         LatLng car1 = new LatLng(39.902138, 116.391415);
         LatLng car2 = new LatLng(39.935184, 116.328587);
         LatLng car3 = new LatLng(39.987814, 116.488232);
-        //���������꼯��
+        //出发地坐标集合
         carsLatLng = new ArrayList<>();
         carsLatLng.add(car1);
         carsLatLng.add(car2);
         carsLatLng.add(car3);
 
-        //Ŀ�ĵ�
+        //目的地
         LatLng go1 = new LatLng(39.96782, 116.403775);
         LatLng go2 = new LatLng(39.891225, 116.322235);
         LatLng go3 = new LatLng(39.883322, 116.415619);
-        //Ŀ�ĵ����꼯��
+        //目的地坐标集合
         goLatLng = new ArrayList<>();
         goLatLng.add(go1);
         goLatLng.add(go2);
@@ -159,58 +159,58 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        //��activityִ��onDestroyʱִ��mMapView.onDestroy()�����ٵ�ͼ
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
         mMapView.onDestroy();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        //��activityִ��onResumeʱִ��mMapView.onResume ()�����»��Ƽ��ص�ͼ
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
         mMapView.onResume();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        //��activityִ��onPauseʱִ��mMapView.onPause ()����ͣ��ͼ�Ļ���
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
         mMapView.onPause();
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        //��activityִ��onSaveInstanceStateʱִ��mMapView.onSaveInstanceState (outState)�������ͼ��ǰ��״̬
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
         mMapView.onSaveInstanceState(outState);
     }
 
-    //ƽ���ƶ�
+    //平滑移动
     public void movePoint(BitmapDescriptor bitmap) {
-        // ��ȡ�켣�����
+        // 获取轨迹坐标点
         List<LatLng> points = readLatLngs();
 //        LatLngBounds bounds = new LatLngBounds(points.get(0), points.get(points.size() - 2));
 //        aMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 50));
 
-//        SparseArrayCompat sparseArrayCompat = new SparseArrayCompat();//�ȸ��¼��ϣ����hashmap
+//        SparseArrayCompat sparseArrayCompat = new SparseArrayCompat();//谷歌新集合，替代hashmap
         SmoothMoveMarker smoothMarker = new SmoothMoveMarker(aMap);
         smoothMarkers.add(smoothMarker);
         int num = smoothMarkers.size() - 1;
-        // ���û�����ͼ��
+        // 设置滑动的图标
         smoothMarkers.get(num).setDescriptor(bitmap);
         LatLng drivePoint = points.get(0);
         Pair<Integer, LatLng> pair = SpatialRelationUtil.calShortestDistancePoint(points, drivePoint);
         points.set(pair.first, drivePoint);
         List<LatLng> subList = points.subList(pair.first, points.size());
 
-        // ���û����Ĺ켣��ߵ�
+        // 设置滑动的轨迹左边点
         smoothMarkers.get(num).setPoints(subList);
-        // ���û�������ʱ��
+        // 设置滑动的总时间
         smoothMarkers.get(num).setTotalDuration(10);
-        // ��ʼ����
+        // 开始滑动
         smoothMarkers.get(num).startSmoothMove();
     }
 
-    //��ȡ·��
+    //获取路线
     private List<LatLng> readLatLngs() {
         List<LatLng> points = new ArrayList<LatLng>();
         for (int i = 0; i < coords.length; i += 2) {
